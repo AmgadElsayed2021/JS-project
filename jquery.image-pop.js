030e1d0ad4efbd23c2eed350c21606c3a53306f4
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-11-15 21:17:28 -0330

    start working on the new style JS

--- jquery.image-pop.js
+++ jquery.image-pop.js
@@ -1,209 +1,30 @@
 (function ($) {
-  $.fn.imagePopup = function (options) {
-    let settings = $.extend(
-      {
-        overlay: "rgba(0,0,0,0.5)",
-        closeButton: {
-          src: null,
-          width: "30px",
-          height: "30px",
-        },
+  let index = "slider-index";
+  let list = {};
+  // generate a random id
+  function createID() {
+    return Math.ceil(Math.random() * 10).toString();
+  }
 
-        imageBorder: "5px solid #ffffff",
-        borderRadius: "5px",
-        imageWidth: "500px",
-        imageHeight: "400px",
-        imageCaption: {
-          exist: true,
-          color: "#fffffff",
-          fontSize: "20px",
-        },
-        RightNavBtn: {
-          src: null,
-          width: "50px",
-          height: "50px",
-        },
-        LeftNavBtn: {
-          src: null,
-          width: "50px",
-          height: "50px",
-        },
-        open: null,
-        close: null,
-        right: null,
-        left: null,
-      },
-      options
-    );
-    /**
-     * Iterating through each image of gallery
-     */
-    return this.each(function () {
-      // declaring new element variable
-      let $overLay,
-        $closeButton,
-        $image,
-        $imageCaption,
-        $RightNavBtn,
-        $LeftNavBtn;
-      //   call the functions
-
-      setOverLayProperties();
-      setCloseBtnProperties();
-      setImageProperties();
-
-      $(this)
-        .find("a")
-        .on("click", function (event) {
-          event.preventDefault();
-          const imageSrc = $(this).children("img").attr("src");
-          $image.attr("src", imageSrc);
-          if (settings.imageCaption.exist == true) {
-            const caption = $(this).children("img").attr("alt");
-            $imageCaption.text(caption);
-          }
-          if ($.isFunction(settings.open)) {
-            settings.open.call(this);
-          }
-          $overLay.css({ opacity: 0.1 }).show().animate({ opacity: 1 });
-        });
-
-      function setImageProperties() {
-        setLeftNavBtnProperties();
-
-        $image = $("<img>");
-        $image.css({
-          width: settings.imageWidth,
-          height: settings.imageHeight,
-          border: settings.imageBorder,
-          "border-radius": settings.borderRadius,
-        });
-        $overLay.append($image);
-        setRightNavBtnProperties();
-
-        if (settings.imageCaption.exist == true) {
-          $imageCaption = $("<p></p>");
-          $imageCaption.css({
-            color: settings.imageCaption.color,
-            "font-size": settings.imageCaption.fontSize,
-          });
-          $overLay.append($imageCaption);
-        }
+  /*
+   i just got it from https://api.jquery.com/data/ and saw some people using it on you tube .
+   The .data() method allows us to attach data of any type to DOM elements in a
+   way that is safe from circular references and therefore from memory leaks.
+*/
+  $.fn.imageSlider = function (options) {
+    this.filter(".photo-slider").each(function () {
+      let sliderContainer = $(this);
+      let uid = createID();
+      const settings = $.extend({}, $.fn.imageSlider.settingsDefault, options);
+      let tmpIx = sliderContainer.data(index);
+      if (tmpIx !== undefined) {
+        settings.slideStartIndex = tmpIx;
       }
 
-      function setOverLayProperties() {
-        $overLay = $("<div></div>");
-        $overLay.css({
-          background: settings.overlay,
-          position: "absolute",
-          top: "0px",
-          left: "0px",
-          display: "none",
-          "text-align": "center",
-          width: "100%",
-          height: "100%",
-          "padding-top": "5%",
-        });
-        $("body").append($overLay);
-      }
-
-      function setCloseBtnProperties() {
-        const prop = {
-          color: "white",
-          cursor: "pointer",
-          "font-size": "20px",
-          width: settings.closeButton.width,
-          height: settings.closeButton.height,
-          position: "absolute",
-          top: "5px",
-          right: "5px",
-          border: "0px",
-          "z-index": "1",
-        };
-        if (settings.closeButton.src) {
-          $closeButton = $("<img>");
-          $closeButton.attr("src", settings.closeButton.src);
-        } else {
-          $closeButton = $("<span>X</span>");
-        }
-        $closeButton.css(prop);
-        $overLay.append($closeButton);
-      }
-      $closeButton.click(function () {
-        if ($.isFunction(settings.close)) {
-          settings.close.call(this);
-        }
-        $overLay.animate({ opacity: 0.1 }, function () {
-          $overLay.hide();
-        });
-      });
-      function setRightNavBtnProperties() {
-        const prop = {
-          color: "white",
-          cursor: "pointer",
-          "font-size": "20px",
-          width: settings.RightNavBtn.width,
-          height: settings.RightNavBtn.height,
-          position: "center",
-          top: "5px",
-          right: "5px",
-          border: "0px",
-          "z-index": "1",
-        };
-        if (settings.RightNavBtn.src) {
-          $RightNavBtn = $("<img>");
-          $RightNavBtn.attr("src", settings.RightNavBtn.src);
-        } else {
-          $RightNavBtn = $("<span>></span>");
-        }
-        $RightNavBtn.css(prop);
-        $overLay.append($RightNavBtn);
-      }
-      $RightNavBtn.click(function () {
-        if ($.isFunction(settings.right)) {
-          settings.right.call(this);
-        }
-        $overLay.animate({ opacity: 0.1 }, function () {
-          $overLay.hide();
-        });
-      });
-      function setLeftNavBtnProperties() {
-        const prop = {
-          color: "white",
-          cursor: "pointer",
-          "font-size": "20px",
-          width: settings.LeftNavBtn.width,
-          height: settings.LeftNavBtn.height,
-          position: "center",
-          top: "5px",
-          right: "5px",
-          border: "0px",
-          "z-index": "1",
-        };
-        if (settings.LeftNavBtn.src) {
-          $LeftNavBtn = $("<img>");
-          $LeftNavBtn.attr("src", settings.LeftNavBtn.src);
-        } else {
-          $LeftNavBtn = $("<span><</span>");
-        }
-        $LeftNavBtn.css(prop);
-        $overLay.append($LeftNavBtn);
-      }
-      $LeftNavBtn.click(function () {
-        // event.preventDefault();
-        const imageSrc = $(this).children("img").prev().attr("src");
-        const next = imageSrc.next();
-        console.log(next.attr("alt"));
-        $image.attr("src", imageSrc);
-        if (settings.imageCaption.exist == true) {
-          const caption = $(this).children("img").prev().attr("alt");
-          $imageCaption.text(caption);
-        }
-        if ($.isFunction(settings.open)) {
-          settings.open.call(this);
-        }
-        $overLay.css({ opacity: 0.1 }).show().animate({ opacity: 1 });
-      });
+      list[uid] = {
+        sliderContainer: sliderContainer,
+        settings: settings,
+      };
+      init(uid);
     });
   };
-})(jQuery);
